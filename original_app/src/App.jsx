ab560a1ed0bd6915b9dc0dee5245aeab2c1824e6
ngsw <ngsw993@gmail.com>
2025-02-21 21:18:54 +0900

    追記

--- original_app/src/App.jsx
+++ original_app/src/App.jsx
@@ -1,6 +1,9 @@
 import { useState } from 'react'
 import './App.css'
 import image_tree from './assets/top/tree.svg'
+import image_lpic from './assets/icon/Icon_LPIC.svg'
+import image_python from './assets/icon/Icon_Python.svg'
+import image_vba from './assets/icon/Icon_VBA.svg'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -21,9 +24,18 @@ function App() {
           <div className='side-container-qualifications'>
             <p>問題集を選ぶ</p>
             <ul>
-              <li>Python</li>
-              <li>VBA</li>
-              <li>LPIC</li>
+              <li>
+                <img src={image_python}/>
+                Python
+              </li>
+              <li>
+                <img src={image_vba}/>
+                VBA
+              </li>
+              <li>
+                <img src={image_lpic}/>
+                LPIC
+              </li>
             </ul>
           </div>
 
