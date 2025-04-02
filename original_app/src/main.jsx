60c2a0664c4a9d1ceb060e794532354c2ede248d
ngsw <ngsw993@gmail.com>
2025-04-03 00:11:59 +0900

    公開設定

--- original_app/src/main.jsx
+++ original_app/src/main.jsx
@@ -1,24 +1,12 @@
-// import { StrictMode } from 'react'
-// import { createRoot } from 'react-dom/client'
-// import React from 'react';
-// import {BrowserRouter} from 'react-router-dom'
-// import {AppRoutes} from './Routes.jsx'
-// import './index.css'
-
-// createRoot(document.getElementById('root')).render(
-//   <StrictMode>
-//     <BrowserRouter>
-//       <AppRoutes />
-//     </BrowserRouter>
-//   </StrictMode>
-// )
-
 import React from "react";
-import ReactDOM from "react-dom/client";
+import  ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";  // Appコンポーネントを読み込む
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename="/react-app">
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
